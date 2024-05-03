f1c35b5720cb0628fb5c3e67f62b50e7c8bacc17
Bohdan337 <bogdansem37@gmail.com>
2024-05-03 17:54:36 +0300

    created a ‘user’ application where models were added

---
+++ user/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, Teacher, ChiefTeacher, Student
+
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Teacher)
+admin.site.register(ChiefTeacher)
+admin.site.register(Student)
