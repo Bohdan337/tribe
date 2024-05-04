9146ed431885e06181331e859f8a90b8383af570
Nazar Svystula <nazarsvistula11@gmail.com>
2024-05-04 21:55:15 +0300

    created urls for registration

--- user/views.py
+++ user/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello world')
+def login(request):
+    form = ''
 
+    return render(request, 'registration/login.html', {'form': form})
 
+def signup(request):
+    form = ''
+
+    return render(request, 'registration/signup.html', {'form': form})
+
+def logout(request):
+    from django.contrib.auth import logout
+    logout(request)
+
+    return redirect('homepage')
