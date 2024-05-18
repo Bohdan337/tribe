9fe5c3e35a46fbb2c0e1aeefe88a37ceb6564df4
Ramieeeeeee <shamapas@gmail.com>
2024-05-18 20:07:32 +0300

    grade class aded

--- user/models.py
+++ user/models.py
@@ -19,11 +19,4 @@ class CustomUser(AbstractUser):
 
     
     
-    
 
-    
-
-'''class Grade(models.Model):
-    grade_id=models.CharField(max_length=245, null=False, blank=False)
-    grade_tittle=models.CharField(max_length=245, null=False, blank=False)
-    grade_leader=models.ForeignKey(Teacher, on_delete=models.CASCADE)'''
