5e40d2d9585491eb303fc50fded046a1f479c11c
Nazar Svystula <nazarsvistula11@gmail.com>
2024-05-18 20:06:25 +0300

    password input added

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import CustomUser, Student, Teacher, ChiefTeacher
 from django.contrib.auth.forms import UserCreationForm
-from django.forms.widgets import Input
+from django.forms.widgets import Input, PasswordInput
 
 
 
@@ -9,8 +9,8 @@ class CustomUserCreationForm(UserCreationForm, forms.Form):
     name = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Name'}))
     surname = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Surname'}))
     email = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Email'}))
-    password = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Password'}))
-    password2 = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Check password'}))
+    password = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Check password'}))
 
     class Meta:
         model = CustomUser
