84de1b9ab09b6c77117239711a6ee3ce9784e726
Nazar Svystula <nazarsvistula11@gmail.com>
2024-05-22 17:52:49 +0300

    Form styles

--- user/forms.py
+++ user/forms.py
@@ -8,11 +8,11 @@ from captcha.fields import CaptchaField
 
 
 class CustomUserCreationForm(UserCreationForm, forms.Form):
-    name = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Name'}))
-    surname = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Surname'}))
-    email = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Email'}))
-    password1 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Password'}))
-    password2 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Check password'}))
+    name = forms.CharField(max_length=255, widget=Input(attrs={'class': 'marg m-3 h-12 rounded-lg p-4 bg-gray-900 text-white', 'placeholder': 'Name'}))
+    surname = forms.CharField(max_length=255, widget=Input(attrs={'class': 'marg m-3 h-12 rounded-lg p-4 bg-gray-900 text-white', 'placeholder': 'Surname'}))
+    email = forms.CharField(max_length=255, widget=Input(attrs={'class': 'marg m-3 h-12 rounded-lg p-4 bg-gray-900 text-white', 'placeholder': 'Email'}))
+    password1 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'marg m-3 h-12 rounded-lg p-4 bg-gray-900 text-white', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'marg m-3 h-12 rounded-lg p-4 bg-gray-900 text-white', 'placeholder': 'Check password'}))
 
     class Meta:
         model = CustomUser
