0d51ad1eafa21f7fe3db3113d0fbfc79e102787a
Nazar Svystula <nazarsvistula11@gmail.com>
2024-05-25 19:02:15 +0300

    Created course creation form

--- user/forms.py
+++ user/forms.py
@@ -11,7 +11,7 @@ from django_recaptcha.widgets import ReCaptchaV2Checkbox
 class CustomUserCreationForm(UserCreationForm, forms.Form):
     name = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Name'}))
     surname = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Surname'}))
-    email = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Email'}))
+    email = forms.CharField(max_length=255, widget=EmailInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Email'}))
     password1 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Password'}))
     password2 = forms.CharField(max_length=255, widget=PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Check password'}))
     captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
