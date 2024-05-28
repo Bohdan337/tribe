0beb90381f31666d5e13af419f4b51c95beb5406
Nazar Svystula <nazarsvistula11@gmail.com>
2024-05-28 21:17:50 +0300

    Created profile image

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,7 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('logout', views.logout, name='logout'),
     path('profile/<slug:slug>', views.profile, name='profile'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
