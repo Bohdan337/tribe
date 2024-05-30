084953668a33a36375c3f5ef1a00f7b6d59d48ab
Ramieeeeeee <shamapas@gmail.com>
2024-05-30 16:56:44 +0300

    account activation added

--- user/views.py
+++ user/views.py
@@ -7,6 +7,9 @@ from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 
 
@@ -36,6 +39,19 @@ def signup(request):
             return redirect('login')
     else:
         form = CustomUserCreationForm()
+        
+
+    email = create_email(
+        request,
+        user,
+        'Activate your user account!',
+        'users/activation.html')
+
+    if email.send():
+         messages.success(request, 'Check your email and confirm your account!')
+    else:
+        messages.error(request, 'Something went wrong with your email. Try it later...')
+        
     return render(request, 'registration/signup.html', {'form': form})
 
 
@@ -67,3 +83,45 @@ def profile(request, slug):
     context = {'profile': profile}
 
     return render(request, 'profile.html', context=context)
+
+
+
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+
+
+def create_email(request, user, subject: str, html_page: str):
+    from .token import account_activation_token
+    from django.contrib.sites.shortcuts import get_current_site
+    # subject='Activate your email'
+    message = render_to_string(html_page, {'user': user,
+                                           'domain': get_current_site(request).domain,
+                                           'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                                           'token': account_activation_token.make_token(user),
+                                           'protocol': 'https' if request.is_secure() else 'http'}
+                               )
+
+    text_message = strip_tags(message)
+
+    email = EmailMultiAlternatives(subject, text_message, to=[user.email])
+    email.attach_alternative(message, 'text/html')
+    return email
+
+def activate_user(request, uid, token):
+    from .token import account_activation_token
+    from .models import CustomUser
+    print('User id:', uid, 'secret token', token)
+    try:
+        from django.contrib.auth.models import User
+        uid = force_str(urlsafe_base64_decode(uid))
+        user = CustomUser.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Your account has been activated successfully!")
+        redirect('/login')
+    else:
+        messages.success(request, "Invalid activation link")
+    return redirect('homepage')
