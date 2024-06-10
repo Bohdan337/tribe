faaa3127cad370b7cfa0f5254d3cfe2e2b47f643
Bohdan337 <bogdansem37@gmail.com>
2024-06-10 16:28:17 +0300

    change display form for subjects

--- subject/forms.py
+++ subject/forms.py
@@ -9,11 +9,10 @@ from tinymce.widgets import TinyMCE
 class CourseForm(forms.ModelForm, forms.Form):
     title = forms.CharField(max_length=255, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Title'}))
     summary = forms.CharField(max_length=1024, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-gray-900 text-white', 'placeholder': 'Description'}))
-    students = forms.ModelMultipleChoiceField(queryset=CustomUser.objects.filter(is_student=True, is_superuser=False), widget=forms.CheckboxSelectMultiple())
 
     class Meta:
         model = Subject
-        fields = ['title', 'summary', 'students']
+        fields = ['title', 'summary']
 
 
 
